e7d2d653e3e30168cd39e7dcdd1bcd2ef905d791
Jeffrey Eesley <jeffeesley@gmail.com>
2017-07-12 11:28:51 -0400

    first commit

---
+++ project2 test/public/js/signup.js
@@ -0,0 +1,50 @@
+$(document).ready(function() {
+  // Getting references to our form and input
+  var signUpForm = $("form#signup");
+  var emailInput = $("input#email-input");
+  var passwordInput = $("input#password-input");
+  var firstNameInput = $("input#firstName-input");
+  var lastNameInput = $("input#lastName-input");
+  var heightInput = $("input#height-input");
+  var currentWeightInput = $("currentWeight-input");
+  var goalWeightInput = $("goalWeight-input");
+
+  // When the signup button is clicked, we validate the email and password are not blank
+  signUpForm.on("submit", function(event) {
+    event.preventDefault();
+    var userData = {
+      email: emailInput.val().trim(),
+      password: passwordInput.val().trim(),
+      firstName: firstNameInput.val().trim(),
+      lastName: firstNameInput.val().trim(),
+      height: heightInput.val().trim(),
+      currentWeight: currentWeightInput.val().trim(),
+      goalWeight: goalWeightInput.val().trim()
+    };
+
+    if (!userData.email || !userData.password) {
+      console.log("wrong");
+      return;
+    }
+    // If we have an email and password, run the signUpUser function
+    signUpUser(userData.email, userData.password);
+    emailInput.val("");
+    passwordInput.val("");
+  });
+
+  
+  function signUpUser(email, password) {
+    $.post("/api/signup", {
+      email: email,
+      password: password
+    }).then(function(data) {
+      window.location.replace(data);
+      // If there's an error, handle it by throwing up a boostrap alert
+    }).catch(handleLoginErr);
+  }
+
+  function handleLoginErr(err) {
+    $("#alert .msg").text(err.responseJSON);
+    $("#alert").fadeIn(500);
+  }
+});
