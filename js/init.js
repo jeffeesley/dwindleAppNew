982a22e446c48af36f8eb25e0771a4d78870e6d8
erinlevine <erinclevine@gmail.com>
2017-07-12 11:18:29 -0400

    Adding my files to GitHub

---
+++ js/init.js
@@ -0,0 +1,13 @@
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+    $('.parallax').parallax();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
+
+// MODALS:
+$(document).ready(function() {
+    $('#modal1').modal();
+});
